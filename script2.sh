3af9c6058bd9bf89c99d8be4c1f04782efa72aba
Paul Aznar <146083581+Aznar7@users.noreply.github.com>
2024-11-13 17:22:15 +0100

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,88 @@
+#1
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-05-26" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-02" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-09" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-16" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-23" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-05-27" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-03" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-10" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-17" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-24" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-18" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-25" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-12" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-19" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-13" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-20" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-07" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-14" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-08" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-06-15" # 
+
+
+#2
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-07" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-14" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-21" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-28" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-04" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-08" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-15" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-22" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-29" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-05" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-30" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-06" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-24" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-31" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-25" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-01" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-19" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-26" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-20" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-07-27" # 
+
+
+#3
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-18" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-25" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-01" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-08" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-15" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-19" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-26" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-02" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-09" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-16" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-10" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-17" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-04" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-11" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-05" # 
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-12" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-30" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-06" # 
+
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-08-31" #
+echo "." >> README.md && git add * && git commit -m "add ." --date="2024-09-07" # 
